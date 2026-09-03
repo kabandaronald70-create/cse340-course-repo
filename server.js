1dd9b327607b98978b22a6ed3020b0f575b71df3
kabandaronald70-create <kabandaronal70@gmail.com>
2026-09-03 22:11:49 +0300

    changes in the categories page

--- server.js
+++ server.js
@@ -50,3 +50,7 @@ app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
 });
+
+app.get('/categories', (req, res) => {
+    res.render('categories', { title: 'Categories' });
+});
