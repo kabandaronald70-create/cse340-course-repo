072c2f94328e2d00a781c36f447ee39936d273cc
kabandaronald70-create <kabandaronal70@gmail.com>
2026-09-08 17:17:58 +0300

    changes in footer and server.js

--- server.js
+++ server.js
@@ -41,6 +41,19 @@ app.listen(PORT, async () => {
   }
 });
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
+
 /**
  * Routes
  */
